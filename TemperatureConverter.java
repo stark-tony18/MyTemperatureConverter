4e6859f228ee04a2190d45986d72c76de8982ca6
stark-tony18 <kunalgaikwad00012@gmail.com>
2023-12-04 15:27:11 +0530

    Modified Temp Conv

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -4,25 +4,21 @@ public class TemperatureConverter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Accept temperature value
+        
         System.out.print("Enter temperature value: ");
         double temperature = scanner.nextDouble();
 
-        // Accept temperature unit (Celsius or Fahrenheit)
+        
         System.out.print("Enter temperature unit (C or F): ");
         char unit = scanner.next().charAt(0);
 
-        // Convert and display temperature in the other unit
+       
         if (unit == 'C' || unit == 'c') {
             double convertedTemperature = celsiusToFahrenheit(temperature);
             System.out.println(temperature + " Celsius is equal to " + convertedTemperature + " Fahrenheit.");
        
 }
-/*
-else if (unit == 'F' || unit == 'f') {
-            double convertedTemperature = fahrenheitToCelsius(temperature);
-            System.out.println(temperature + " Fahrenheit is equal to " + convertedTemperature + " Celsius.");
-        }*/
+
 else {
             System.out.println("Invalid temperature unit. Please enter C or F.");
         }
